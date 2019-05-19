13c5a4a9c9fc28be630266fc9551da3dc62fc433
anniebaker <bakeranneabb@gmail.com>
2019-05-19 13:28:54 -0500

    file push

--- file.js
+++ file.js
@@ -1,6 +1,17 @@
-require('dotenv').config()
-console.log(process.file.env.KEY);
+require("dotenv").config();
+console.log(process.env.KEY);
 
+const baseURL = "https://sandbox-api.brewerydb.com/v2/";
+
+fetch(baseURL + "locations/?key=" + process.env.KEY)
+  .then(res => {
+    if (!res.ok) {
+      throw Error(res.statusText);
+    }
+    return res.json();
+  })
+  .then(posts => (arrayOfPosts = posts))
+  .catch(err => console.log(`Error, ${err}`));
 /*
 
 */
